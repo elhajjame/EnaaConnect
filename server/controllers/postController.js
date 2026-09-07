6e355fcf71d4161284d9e4f7b9f291ddfae3de9c
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-09-07 13:29:56 +0100

    feat: add post feed retrieval

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -17,6 +17,7 @@ export const createPost = async (req, res) => {
         id: req.user._id,
         fullName: req.user.fullName,
         profilePicture: req.user.profilePicture,
+        fieldOfStudy: req.user.fieldOfStudy,
       },
       likesCount: post.likes.length,
       commentsCount: post.comments.length,
@@ -24,6 +25,36 @@ export const createPost = async (req, res) => {
     };
 
     return successResponse(res, 201, postData, "Post created successfully");
+  } catch (error) {
+    return handleControllerError(res, error);
+  }
+};
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate("author", "fullName profilePicture fieldOfStudy");
+
+    const postData = posts.map((post) => {
+      return {
+        id: post._id,
+        content: post.content,
+        image: post.image,
+        author: post.author
+          ? {
+              id: post.author._id,
+              fullName: post.author.fullName,
+              profilePicture: post.author.profilePicture,
+              fieldOfStudy: post.author.fieldOfStudy,
+            }
+          : null,
+        likesCount: post.likes.length,
+        commentsCount: post.comments.length,
+        createdAt: post.createdAt,
+      };
+    });
+    return successResponse(res, 200, postData, "Posts retrieved successfully");
   } catch (error) {
     return handleControllerError(res, error);
   }
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createPost } from "../controllers/postController.js";
+import { createPost, getPosts } from "../controllers/postController.js";
 import { validateCreatePost } from "../middleware/postMiddleware.js";
 import protect from "../middleware/protectMiddleware.js";
 import { requireStudent } from "../middleware/roleMiddleware.js";
 
 const route = Router();
+
+route.get("/", protect, getPosts);
 route.post("/", protect, requireStudent, validateCreatePost, createPost);
 export default route;
