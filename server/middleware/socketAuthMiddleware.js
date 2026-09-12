f0d476cd7886b9af0e16b52b76c666f59adf62e4
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-09-12 18:02:20 +0100

    feat(socket): add socket authentication middleware for secure connections

---
+++ server/middleware/socketAuthMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/UserModel.js";
+
+const authenticateSocket = async (socket, next) => {
+  try {
+    const authorizationHeader = socket.handshake.headers.authorization;
+
+    let token = socket.handshake.auth?.token;
+
+    if (!token && authorizationHeader?.startsWith("Bearer ")) {
+      token = authorizationHeader.split(" ")[1];
+    }
+    console.log("token", token);
+    if (!token) {
+      return next(new Error("Authentication required"));
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("decoded", decoded);
+    const user = await User.findById(decoded.id);
+    console.log("user", user);
+    if (!user) {
+      return next(new Error("User no longer exists"));
+    }
+
+    socket.user = user;
+
+    next();
+  } catch (error) {
+    return next(new Error("Invalid or expired token"));
+  }
+};
+export default authenticateSocket;
--- server/server.js
+++ server/server.js
@@ -4,15 +4,17 @@ import connectDB from "./config/db.js";
 dotenv.config({ path: ".env" });
 import { Server } from "socket.io";
 import { createServer } from "http";
+import authenticateSocket from "./middleware/socketAuthMiddleware.js";
 
 const port = process.env.PORT || 3000;
 
-const clientUrl = process.env.CLIENT_URL;
+const clientUrl = process.env.CLIENT_URL
 
 const httpServer = createServer(app);
 
 //here is the websocket configured Server is a build in class of websocket.io || httpserver native http server by passing it I said to the socket
 // look listen on the incomin http req on this server when u see a client Asking for a web socket take the connection
+
 const io = new Server(httpServer, {
   cors: {
     origin: clientUrl,
@@ -25,6 +27,8 @@ app.set("io", io);
 
 //socket is an object represent on client active connection it automaticly creates by socket.io
 // internal engine so when a user connected socket.io pass the connection object to the callback function as a argement
+
+io.use(authenticateSocket);
 io.on("connection", (socket) => {
   console.log(`socket connected ${socket.id}`);
 
