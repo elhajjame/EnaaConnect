8b96aeb6110f4c6064cff9dbca9273f4d33d33db
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-09-20 18:00:00 +0100

    feat(auth): add protected and guest routes

---
+++ client/src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+const ProtectedRoute = () => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return null;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="login" replace />;
+  }
+  return <Outlet />;
+};
+export default ProtectedRoute;
